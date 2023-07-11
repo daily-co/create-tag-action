53781e3becca89e11531f5a8e71eff2e8ce0642e
Brian Mathiyakom <brian@mathiyakom.net>
2023-07-11 13:59:05 -0700

    Add debug logging that will be removed later

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 function stringifyDate(date) {
   return date.toISOString().substring(0, 10);
@@ -12,17 +12,18 @@ function getTagName(appName, environment) {
 
 async function run() {
   try {
-    const appName = core.getInput('app-name');
+    console.log("Brian and Wyatt were here lolz.");
+    const appName = core.getInput("app-name");
     if (!appName) {
       core.setFailed('Input "app-name" is missing');
     }
 
-    const environment = core.getInput('environment');
+    const environment = core.getInput("environment");
     if (!environment) {
       core.setFailed('Input "environment" is missing');
     }
 
-    let githubToken = core.getInput('github-token');
+    let githubToken = core.getInput("github-token");
     if (!githubToken) {
       if (process.env.GITHUB_TOKEN) {
         githubToken = process.env.GITHUB_TOKEN;
@@ -36,11 +37,11 @@ async function run() {
     const octokit = github.getOctokit(githubToken);
 
     const tagName = getTagName(appName, environment);
-    core.setOutput('tag', tagName);
+    core.setOutput("tag", tagName);
     const tagMessage = `${tagName} deployed via GitHub Actions`;
 
     const { owner, repo } = github.context.repo;
-    const sha = core.getInput('sha') || process.env.GITHUB_SHA;
+    const sha = core.getInput("sha") || process.env.GITHUB_SHA;
     if (!sha) {
       core.setFailed(
         'SHA to tag must be provided as input "sha" or environment variable "GITHUB_SHA"'
@@ -53,10 +54,10 @@ async function run() {
       tag: tagName,
       message: tagMessage,
       object: sha,
-      type: 'commit',
+      type: "commit",
     });
 
-    console.log('Tag created successfully.');
+    console.log("Tag created successfully.");
 
     const refName = `tags/${tagName}`;
     let refExists = false;
@@ -68,7 +69,7 @@ async function run() {
         ref: refName,
       });
       refExists = true;
-      console.log('Existing ref found.  Will update.');
+      console.log("Existing ref found.  Will update.");
     } catch (error) {
       if (error.status != 404) {
         throw error;
@@ -85,7 +86,7 @@ async function run() {
         ref: refName,
         sha,
       });
-      console.log('Ref updated.');
+      console.log("Ref updated.");
     } else {
       // Create new ref
       const fullRefName = `refs/${refName}`;
@@ -95,7 +96,7 @@ async function run() {
         ref: fullRefName,
         sha,
       });
-      console.log('Ref created.');
+      console.log("Ref created.");
     }
   } catch (error) {
     core.setFailed(error.message);
